c5f9e1066e5edd6a71cf4095d83ebe3c1d8fd8b9
DemonLostKotlin <hpsousa100@gmail.com>
2023-05-01 14:29:23 -0300

    terminal

---
+++ src/main/java/br/edu/henrique/dio/bootcamp/argumentos/MinhaClasse.java
@@ -0,0 +1,7 @@
+package br.edu.henrique.dio.bootcamp.argumentos;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Olá, fui executado pelo terminal!");
+    }
+}
