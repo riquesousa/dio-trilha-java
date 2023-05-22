2ee0c7d2a712d2dfb526e4fa6f0dd93807651db2
DemonLostKotlin <hpsousa100@gmail.com>
2023-05-21 21:51:29 -0300

    aula 21_05

---
+++ src/main/java/br/edu/henrique/dio/bootcamp/poo/RodarAplicacao.java
@@ -0,0 +1,24 @@
+package br.edu.henrique.dio.bootcamp.poo;
+
+public class RodarAplicacao {
+    public static void main(String[] args) {
+
+        Carro carro1 = new Carro();
+
+        carro1.setCor("Azul");
+        carro1.setModelo("BMW X1");
+        carro1.setCapacidadeTanque(50);
+
+        System.out.println(carro1.getModelo());
+        System.out.println(carro1.getCor());
+        System.out.println(carro1.getCapacidadeTanque());
+        System.out.println(carro1.valorTotalTanque(5.40));
+
+        Carro carro2 = new Carro("Vermelho", "Audi A3", 46);
+
+        System.out.println(carro2.getModelo());
+        System.out.println(carro2.getCor());
+        System.out.println(carro2.getCapacidadeTanque());
+        System.out.println(carro2.valorTotalTanque(5.40));
+    }
+}
